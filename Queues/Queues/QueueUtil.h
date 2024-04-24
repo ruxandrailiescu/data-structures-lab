adc748d87abd68e61fb71e2a095c7ff9840d3c5a
Ruxandra Iliescu <iliescuruxandra23@stud.ase.ro>
2024-04-24 06:28:32 +0300

    QueueUtil.h

---
+++ Queues/Queues/QueueUtil.h
@@ -0,0 +1,74 @@
+#pragma once
+#include "DataUtil.h"
+
+typedef struct NodeQ
+{
+	Student* info;
+	struct NodeQ* next;
+}QueueNode, *PQueueNode;
+
+QueueNode* createQueueNode(Student* info)
+{
+	QueueNode* node = (QueueNode*)malloc(sizeof(QueueNode));
+	node->info = info;
+	node->next = NULL;
+	return node;
+}
+
+void putNode(QueueNode** tail, Student* stud)
+{
+	QueueNode* node = createQueueNode(stud);
+	if ((*tail) != NULL)
+	{
+		node->next = (*tail);
+	}
+	(*tail) = node;
+}
+
+Student* peekNode(QueueNode* tail)
+{
+	Student* result = NULL;
+	// iterate from tail to head
+	// until head is reached
+	if (tail != NULL)
+	{
+		while (tail->next != NULL)
+		{
+			tail = tail->next;
+		}
+		result = tail->info;
+	}
+	return result;
+}
+
+Student* popNode(QueueNode** tail)
+{
+	Student* result = NULL;
+	if ((*tail) != NULL)
+	{
+		QueueNode* tmp = (*tail);
+		while (tmp->next->next != NULL)
+		{
+			tmp = tmp->next;
+		}
+		// tmp->next->next == NULL
+		// set next pointer of tmp == NULL to remove head
+		result = tmp->next->info;
+		printStudent(result);
+		tmp->next = NULL;
+	}
+	return result;
+}
+
+void displayQueue(QueueNode** tail)
+{
+	Student* info = peekNode(*tail);
+	Student* tmp = NULL;
+	while (tmp != info)
+	{
+		tmp = popNode(tail);
+		putNode(tail, tmp);
+		printStudent(tmp);
+		tmp = peekNode(*tail);
+	}
+}
---
+++ Queues/Queues/Queues.c
@@ -0,0 +1,33 @@
+#include "QueueUtil.h"
+#define LINE_SIZE 256
+
+void main()
+{
+	QueueNode* tailQueue = NULL;
+	FILE* pFile = fopen("Data.txt", "r");
+	if (pFile)
+	{
+		char delimiter[] = ",";
+		char* token = NULL;
+		float income; int index = 0;
+		unsigned short reference;
+		char buffer[LINE_SIZE], name[LINE_SIZE];
+		while (fgets(buffer, sizeof(buffer), pFile))
+		{
+			token = strtok(buffer, delimiter);
+			strcpy(name, token);
+			token = strtok(NULL, delimiter);
+			income = atof(token);
+			token = strtok(NULL, delimiter);
+			reference = atoi(token);
+			Student* stud = createStudent(name, income, reference);
+			putNode(&tailQueue, stud);
+		}
+		displayQueue(&tailQueue);
+		printf("\n\n");
+		while (tailQueue)
+		{
+			popNode(&tailQueue);
+		}
+	}
+}
