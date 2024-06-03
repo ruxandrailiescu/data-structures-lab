d782c4e49f54960e17dd476d4ec2c525b8830c4a
Ruxandra Iliescu <iliescuruxandra23@stud.ase.ro>
2024-06-04 01:28:25 +0300

    DLL

--- ExamPrep/ExamPrep/DLL.c
+++ ExamPrep/ExamPrep/DLL.c
@@ -5,5 +5,43 @@
 
 void main()
 {
+	Node* head = NULL;
+	Node* head2 = NULL;
+	FILE* pFile = fopen("Data.txt", "r");
+	if (pFile)
+	{
+		char delimiter[] = ",";
+		char* token = NULL;
+		float income; int index = 0;
+		unsigned short reference;
+		char buffer[LINE_SIZE], name[LINE_SIZE];
+		while (fgets(buffer, sizeof(buffer), pFile))
+		{
+			token = strtok(buffer, delimiter);
+			strcpy(name, token);
+			token = strtok(NULL, delimiter);
+			income = atof(token);
+			token = strtok(NULL, delimiter);
+			reference = atoi(token);
+			Student* stud = createStudent(name, income, reference);
+			insertTailDL(&head, stud);
+			insertTailDL(&head2, stud);
+		}
+	}
 
+	Student* stud1 = createStudent("Ruxandra Iliescu", 5000, 23);
+	insertAfterNodeDL(&head, stud1, 78);
+	deleteNodeByKeyDL(&head, 23);
+	//deleteListDL(&head);
+
+	//while (head)
+	//{
+	//	printStudent(head->info);
+	//	head = head->next;
+	//}
+
+	head = createCircularDL(head);
+	head2 = createCircularDL(head2);
+	head = mergeCircularListsDL(head, head2);
+	displayListBothWaysCircular(head);
 }
--- ExamPrep/ExamPrep/DListUtil.h
+++ ExamPrep/ExamPrep/DListUtil.h
@@ -54,15 +54,16 @@ void insertAfterNodeDL(Node** head, Student* stud, unsigned char key)
 	else
 	{
 		Node* tmp = *head;
-		while (tmp->next)
+		while (tmp)
 		{
 			if (tmp->info->reference.intRef == key)
 			{
-				Node* aux = tmp;
+				Node* nextNode = tmp->next;
 				tmp->next = node;
 				node->prev = tmp;
-				node->next = aux->next->next;
-				aux->next->next->prev = node;
+				node->next = nextNode;
+				if (nextNode)
+					nextNode->prev = node;
 				return;
 			}
 			tmp = tmp->next;
@@ -71,8 +72,119 @@ void insertAfterNodeDL(Node** head, Student* stud, unsigned char key)
 }
 
 // delete node
+void deleteNodeByKeyDL(Node** head, unsigned char key)
+{
+	if ((*head) == NULL)
+		return;
+	
+	Node* tmp = *head;
+
+	if (tmp->info->reference.intRef == key)
+	{
+		*head = tmp->next;
+		if (*head)
+			(*head)->prev = NULL;
+		deleteStudent(tmp->info);
+		free(tmp);
+		return;
+	}
+
+	while (tmp)
+	{
+		if (tmp->info->reference.intRef == key)
+		{
+			Node* prev = tmp->prev;
+			Node* next = tmp->next;
+
+			if (prev)
+				prev->next = next;
+
+			if (next)
+				next->prev = prev;
+
+			deleteStudent(tmp->info);
+			free(tmp);
+			return;
+		}
+		tmp = tmp->next;
+	}
+}
+
 // delete list
-// concatenate list
+void deleteListDL(Node** head)
+{
+	while (*head)
+	{
+		Node* tmp = *head;
+		(*head) = tmp->next;
+		if(*head)
+			(*head)->prev = NULL;
+		deleteStudent(tmp->info);
+		free(tmp);
+	}
+}
+
 // create circular list
+Node* createCircularDL(Node* head)
+{
+	if (head == NULL)
+		return NULL;
+
+	Node* tmp = head;
+	while (tmp->next)
+		tmp = tmp->next;
+
+	tmp->next = head;
+	head->prev = tmp;
+
+	return head;
+}
+
 // merge circular lists
+Node* mergeCircularListsDL(Node* list1, Node* list2)
+{
+	if (list1 == NULL)
+		return list2;
+
+	if (list2 == NULL)
+		return list1;
+
+	Node* tail1 = list1;
+	while (tail1->next != list1)
+		tail1 = tail1->next;
+
+	Node* tail2 = list2;
+	while (tail2->next != list2)
+		tail2 = tail2->next;
+
+	tail1->next = list2;
+	list2->prev = tail1;
+
+	tail2->next = list1;
+	list1->prev = tail2;
+
+	return list1;
+}
+
 // display circular list
+void displayListBothWaysCircular(Node* head)
+{
+	Node* current = head;
+	if (current != NULL)
+	{
+		do
+		{
+			printStudent(current->info);
+			current = current->next;
+		} while (current != head);
+
+		Node* tail = head->prev;
+		printf("--------Reverse order-----------\n");
+		do
+		{
+			printStudent(tail->info);
+			tail = tail->prev;
+		} while (tail != head);
+		printStudent(tail->info);
+	}
+}
