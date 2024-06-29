18c48e92840284efff9a858940f51a828155725f
Ruxandra Iliescu <iliescuruxandra23@stud.ase.ro>
2024-06-29 16:20:17 +0300

    Bilet2 Iunie 2024 (partial)

--- ExamROJune/ExamROJune/Reteta.c
+++ ExamROJune/ExamROJune/Reteta.c
@@ -1,10 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS
 #define LINE_SIZE 256
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-// 1.
+// 1. Crearea structurii Reteta si a Listei Duble
+//    Operatii: creareReteta, afisareReteta, stergereReteta
+//				creareNod, inserareNodPozitie, inserareNodSortat, stergereNod
+//				afisareLista
+
 typedef struct Reteta
 {
 	unsigned int nrReteta;
@@ -13,152 +18,48 @@ typedef struct Reteta
 	char* statutSpecial;
 	unsigned char nrMedicamente;
 	float procentCompensare;
-	//int* aaa;
-	//int nrAaa;
 } Reteta;
 
-typedef struct NodDLL
+Reteta* creareReteta(unsigned int _nrReteta, const char* _numePacient, const char* _numeMedic,
+	const char* _statutSpecial, unsigned char _nrMedicamente, float _procentCompensare)
 {
-	struct NodDLL* prev;
-	Reteta* info;
-	struct NodDLL* next;
-} Nod;
+	// 1. validare (optional)
+	if (_numePacient == NULL || _numeMedic == NULL || _statutSpecial == NULL)
+	{
+		return NULL;
+	}
 
-Reteta* creareReteta(unsigned int _nrReteta, const char* _numePacient, const char* _numeMedic, const char* _statutSpecial, unsigned char _nrMedicamente, float _procentCompensare/*, int* _aaa, int _nrAaa*/)
-{
-	// 1. alocam memorie pt o reteta
+	// 2. alocare memorie structura
 	Reteta* reteta = (Reteta*)malloc(sizeof(Reteta));
-	
-	// 2. initializam atributele
+
+	// 3. initializare atribute
 	reteta->nrReteta = _nrReteta;
+	
 	reteta->numePacient = (char*)malloc(strlen(_numePacient) + 1);
 	strcpy(reteta->numePacient, _numePacient);
+
 	reteta->numeMedic = (char*)malloc(strlen(_numeMedic) + 1);
 	strcpy(reteta->numeMedic, _numeMedic);
+
 	reteta->statutSpecial = (char*)malloc(strlen(_statutSpecial) + 1);
 	strcpy(reteta->statutSpecial, _statutSpecial);
+
 	reteta->nrMedicamente = _nrMedicamente;
 	reteta->procentCompensare = _procentCompensare;
 
-	//reteta->aaa = (int*)malloc(sizeof(int)*_nrAaa);
-	//for (int i = 0; i < _nrAaa; i++) {
-	//	reteta->aaa[i] = _aaa[i];
-	//}
-
-	// 3. returnam reteta
+	// 4. return structura
 	return reteta;
 }
 
-void displayReteta(Reteta* reteta)
+void afisareReteta(Reteta* reteta)
 {
 	if (reteta)
 	{
-		printf("Nr reteta: %d, Nume pacient: %s, Nume medic: %s, Statut special: %s, Nr medicamente: %d, Procent compensare: %.2f\n",
+		printf("\nNr reteta: %d, Nume pacient: %s, Nume medic: %s, Statut special: %s, Nr medicamente: %d, Procent compensare: %.2f\n",
 			reteta->nrReteta, reteta->numePacient, reteta->numeMedic, reteta->statutSpecial, reteta->nrMedicamente, reteta->procentCompensare);
 	}
 }
 
-Nod* creareNod(Reteta* reteta)
-{
-	// 1. alocam memorie pt nod
-	Nod* nod = (Nod*)malloc(sizeof(Nod));
-
-	// 2. initializam atributele
-	nod->info = reteta;
-	nod->prev = nod->next = NULL;
-
-	// 3. returnam nodul
-	return nod;
-}
-
-void inserareListaCheie(Nod** lista, Reteta* reteta, unsigned int cheie)
-{
-	// cheie = nr reteta
-
-	Nod* nod = creareNod(reteta);
-
-	Nod* tmp = (*lista);
-
-	if (tmp == NULL)
-	{
-		(*lista) = nod;
-		return;
-	}
-
-	while (tmp)
-	{
-		if (tmp->info->nrReteta == cheie)
-		{
-			if (tmp->next != NULL)
-			{
-				Nod* nodNext = tmp->next;
-				tmp->next = nod;
-				nod->prev = tmp;
-				nod->next = nodNext;
-				nodNext->prev = nod;
-			}
-			else 
-			{
-				tmp->next = nod;
-				nod->prev = tmp;
-			}
-			return;
-		}
-		tmp = tmp->next;
-	}
-}
-
-// traversarea listei in ambele sensuri
-void displayLista(Nod* lista)
-{
-	while (lista->next)
-	{
-		displayReteta(lista->info);
-		lista = lista->next;
-	}
-	displayReteta(lista->info);
-
-	printf("---------- REVERSED ORDER -----------\n");
-	while (lista->prev)
-	{
-		displayReteta(lista->info);
-		lista = lista->prev;
-	}
-	displayReteta(lista->info);
-}
-
-// 2.
-unsigned char calcNrRetete(Nod* lista, unsigned char nrMed)
-{
-	unsigned char nrRetete = 0;
-	while (lista)
-	{
-		if (lista->info->nrMedicamente > nrMed)
-		{
-			nrRetete++;
-		}
-		lista = lista->next;
-	}
-	return nrRetete;
-}
-
-// 3.
-void actualizareProcentCompensare(Nod** lista, const char* numePacient, float procentCompensareNou)
-{
-	Nod* tmp = *lista;
-	while (tmp)
-	{
-		if (strcmp(tmp->info->numePacient, numePacient) == 0)
-		{
-			tmp->info->procentCompensare = procentCompensareNou;
-			break;
-		}
-		tmp = tmp->next;
-	}
-	displayLista(*lista);
-}
-
-// 4.
 void stergereReteta(Reteta* reteta)
 {
 	if (reteta)
@@ -170,44 +71,187 @@ void stergereReteta(Reteta* reteta)
 	}
 }
 
-void stergereNodCheie(Nod** lista, unsigned char nrMed)
+typedef struct NodLD
+{
+	struct NodLD* prev;
+	Reteta* reteta;
+	struct NodLD* next;
+} Nod;
+
+Nod* creareNod(Reteta* reteta)
+{
+	// validare (optional)
+	Nod* nod = NULL;
+	if (reteta)
+	{
+		// 1. alocare memorie structura
+		nod = (Nod*)malloc(sizeof(Nod));
+
+		// 2. initializare atribute
+		nod->reteta = reteta;
+		nod->next = nod->prev = NULL;
+	}
+	// 3. return structura
+	return nod;
+}
+
+void inserareNodPozitie(Nod** lista, Reteta* reteta, int poz)
+{
+	Nod* nod = creareNod(reteta);
+
+	// daca lista este goala
+	if ((*lista) == NULL)
+	{
+		(*lista) = nod;
+	}
+	else
+	{
+		// verificam daca pozitia este admisibila
+		Nod* tmp = (*lista);
+		int counter = 0;
+		while (tmp)
+		{
+			counter++;
+			tmp = tmp->next;
+		}
+
+		// poz = 0 => inseram la head
+		if (poz == 0)
+		{
+			nod->next = (*lista);
+			(*lista)->prev = nod;
+			(*lista) = nod;
+			return;
+		}
+
+		// resetam tmp la inceputul listei
+		tmp = (*lista);
+
+		// poz >= counter => inseram la tail
+		if (poz >= counter)
+		{
+			while (tmp->next)
+			{
+				tmp = tmp->next;
+			}
+			tmp->next = nod;
+			nod->prev = tmp;
+			return;
+		}
+
+		counter = 0;
+		while (counter < poz)
+		{
+			counter++;
+			tmp = tmp->next;
+		}
+		Nod* nxt = tmp;
+		tmp->prev->next = nod;
+		nod->prev = tmp->prev;
+		nxt->prev = nod;
+		nod->next = nxt;
+	}
+}
+
+// (*) aditional: inserare sortata pe baza procentului de compensare
+
+void stergereNod(Nod* nod)
+{
+	if (nod)
+	{
+		stergereReteta(nod->reteta);
+		free(nod);
+	}
+}
+
+void afisareLista(Nod* lista)
+{
+	while (lista->next)
+	{
+		afisareReteta(lista->reteta);
+		lista = lista->next;
+	}
+	afisareReteta(lista->reteta);
+
+	printf("------------------ TRAVERSARE SENS OPUS ------------------");
+	
+	while (lista->prev)
+	{
+		afisareReteta(lista->reteta);
+		lista = lista->prev;
+	}
+	afisareReteta(lista->reteta);
+}
+
+// 2.
+// modificam nr retete in interiorul functiei
+// alta optiune
+//int calcNrRetete(Nod* lista, unsigned char nrMed)
+void calcNrRetete(Nod* lista, int* nrRetete, unsigned char nrMed)
+{
+	while (lista)
+	{
+		if (lista->reteta->nrMedicamente > nrMed)
+		{
+			(*nrRetete)++;
+		}
+		lista = lista->next;
+	}
+}
+
+// 3.
+void actualizareProcentComp(Nod** lista, const char* numePacient, float procentNou)
 {
 	Nod* tmp = (*lista);
 	while (tmp)
 	{
-		if (tmp->info->nrMedicamente < nrMed)
+		if (strcmp(tmp->reteta->numePacient, numePacient) == 0)
 		{
-			if (tmp->prev != NULL)
+			tmp->reteta->procentCompensare = procentNou;
+		}
+		tmp = tmp->next;
+	}
+}
+
+// 4.
+void stergereNodCriteriu(Nod** lista, unsigned char nrMed)
+{
+	Nod* tmp = *lista;
+	
+	while (tmp)
+	{
+		Nod* deSters = tmp;
+		tmp = tmp->next;		// actualizam tmp inainte de a sterge nodul pt a asigura ca accesam memorie valida
+
+		if (deSters->reteta->nrMedicamente < nrMed)
+		{
+			if (deSters->prev)
 			{
-				// actualizam pointerul next al nodului precedent
-				// daca nodul care trebuie sters nu este la inceputul listei
-				tmp->prev->next = tmp->next;
+				deSters->prev->next = deSters->next;
 			}
 			else
 			{
-				(*lista) = tmp->next;
+				// stergem nodul de la inceputul listei
+				(*lista) = deSters->next;
 			}
 
-			if (tmp->next != NULL)
+			if (deSters->next)
 			{
-				// actualizam pointerul prev al nodului urmator
-				// daca nodul care trebuie sters nu este la sfarsitul listei
-				tmp->next->prev = tmp->prev;
+				deSters->next->prev = deSters->prev;
 			}
-			stergereReteta(tmp->info);
-			free(tmp);
+			stergereNod(deSters);
 		}
-		tmp = tmp->next;
 	}
 }
 
 void main()
 {
 	Nod* lista = NULL;
-	FILE* pFile = fopen("Data.txt", "r");
+	FILE* pFile = fopen("DataReteta.txt", "r");
+
 	if (pFile)
 	{
-		char delimiter[] = ",\n";
+		char delimiters[] = ",\n";
 		char* token = NULL;
 		char buffer[LINE_SIZE], numePacient[LINE_SIZE], numeMedic[LINE_SIZE], statutSpecial[LINE_SIZE];
 		unsigned int nrReteta;
@@ -216,37 +260,37 @@ void main()
 
 		while (fgets(buffer, sizeof(buffer), pFile))
 		{
-			token = strtok(buffer, delimiter);
+			token = strtok(buffer, delimiters);
 			nrReteta = atoi(token);
-			token = strtok(NULL, delimiter);
+			token = strtok(NULL, delimiters);
 			strcpy(numePacient, token);
-			token = strtok(NULL, delimiter);
+			token = strtok(NULL, delimiters);
 			strcpy(numeMedic, token);
-			token = strtok(NULL, delimiter);
+			token = strtok(NULL, delimiters);
 			strcpy(statutSpecial, token);
-			token = strtok(NULL, delimiter);
-			nrMedicamente = (char)atoi(token);
-			token = strtok(NULL, delimiter);
+			token = strtok(NULL, delimiters);
+			nrMedicamente = atoi(token);
+			token = strtok(NULL, delimiters);
 			procentCompensare = atof(token);
 
+			// 1.
 			Reteta* reteta = creareReteta(nrReteta, numePacient, numeMedic, statutSpecial, nrMedicamente, procentCompensare);
-			//displayReteta(reteta);
-			inserareListaCheie(&lista, reteta, 1);
+			//afisareReteta(reteta);
+			inserareNodPozitie(&lista, reteta, nrReteta - 1);
 		}
 	}
-	//displayLista(lista);
+	//afisareLista(lista);
 
 	// 2.
-	int nrRetete = calcNrRetete(lista, 3);
-	printf("\nNr de retete care au mai mult de 3 medicamente: %d\n", nrRetete);
+	int nrRetete = 0;
+	calcNrRetete(lista, &nrRetete, 1);
+	printf("\nNr retete: %d\n", nrRetete);
 
 	// 3.
-	actualizareProcentCompensare(&lista, "Ana Boncu", 222.2);
-	printf("\n----------- DISPLAY LISTA MAIN ---------\n");
-	displayLista(lista);
+	actualizareProcentComp(&lista, "Ana Boncu", 45.5);
+	//afisareLista(lista);
 
 	// 4.
-	stergereNodCheie(&lista, 5);
-	printf("\n----------- DISPLAY LISTA DUPA STERGERE ---------\n");
-	displayLista(lista);
+	stergereNodCriteriu(&lista, 5);
+	afisareLista(lista);
 }
