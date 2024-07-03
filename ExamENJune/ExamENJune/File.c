fa4e9e70dfae9273475d57cec48ebde75e963bfa
Ruxandra Iliescu <iliescuruxandra23@stud.ase.ro>
2024-07-03 17:50:32 +0300

    Subject #1E ExamENJune (BST + HEAP)

--- ExamENJune/ExamENJune/File.c
+++ ExamENJune/ExamENJune/File.c
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #define LINE_SIZE 256
+#define PQUEUE_SIZE 7
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -110,7 +111,7 @@ void displayTree(TreeNode* root, int level)
 		{
 			printf("\t");
 		}
-		printf("%s\n", root->info->identifier);
+		printf("%s: %d bytes\n", root->info->identifier, root->info->size);
 		
 		level++;
 		displayTree(root->left, level);
@@ -188,10 +189,188 @@ double avgFileSize(TreeNode* root, int* filesCount)
 }
 
 // 5.
+// function should not visit all nodes (???)
+// function should increase the size of all files if no identifier is found
+void increaseSizePath(TreeNode* root, const char* identifier)
+{
+	if (root != NULL)
+	{
+		if (strcmp(root->info->identifier, identifier) > 0)
+		{
+			increaseSizePath(root->left, identifier);
+		}
+		else
+		{
+			if (strcmp(root->info->identifier, identifier) < 0)
+			{
+				increaseSizePath(root->right, identifier);
+			}
+			else
+			{
+				root->info->size *= 2;
+				return;
+			}
+		}
 
+		if (strcmp(root->info->identifier, identifier) != 0)
+		{
+			root->info->size *= 2;
+		}
+	}
+}
+
+// ADDITIONAL (BST):
+// structure traversal
+void preorder(TreeNode* root)
+{
+	if (root)
+	{
+		displayFile(root->info);
+		preorder(root->left);
+		preorder(root->right);
+	}
+}
+
+void inorder(TreeNode* root)
+{
+	if (root)
+	{
+		inorder(root->left);
+		displayFile(root->info);
+		inorder(root->right);
+	}
+}
+
+void postorder(TreeNode* root)
+{
+	if (root)
+	{
+		postorder(root->left);
+		postorder(root->right);
+		displayFile(root->info);
+	}
+}
+
+// deletion
+void deleteFullNode(TreeNode** root, TreeNode** rSubTree)
+{
+	if ((*rSubTree)->left)
+	{
+		deleteFullNode(root, &(*rSubTree)->left);
+	}
+	else
+	{
+		deleteFile((*root)->info);
+		(*root)->info = (*rSubTree)->info;
+		TreeNode* tmp = *rSubTree;
+		(*rSubTree) = tmp->right;
+		free(tmp);
+	}
+}
+
+void deleteNodeByKey(TreeNode** root, const char* identifier)
+{
+	if (*root != NULL)
+	{
+		if (strcmp((*root)->info->identifier, identifier) > 0)
+		{
+			deleteNodeByKey(&(*root)->left, identifier);
+		}
+		else if (strcmp((*root)->info->identifier, identifier) < 0)
+		{
+			deleteNodeByKey(&(*root)->right, identifier);
+		}
+		else
+		// we found the node
+		{
+			// the node is a leaf
+			if ((*root)->left == NULL && (*root)->right == NULL)
+			{
+				deleteFile((*root)->info);
+				free(*root);
+				*root = NULL;
+			}
+			// the node has 1 descendant
+			else if ((*root)->left == NULL || (*root)->right == NULL)
+			{
+				TreeNode* tmp = *root;
+				*root = (*root)->left ? (*root)->left : (*root)->right;
+				deleteFile(tmp->info);
+				free(tmp);
+			}
+			// the node has 2 descendants
+			else
+			{
+				deleteFullNode(root, &(*root)->right);
+			}
+		}
+	}
+}
+
+// 6.
+// MAX PQueue
+// insert function + reheapUp function
+typedef struct PQueue
+{
+	File** items;
+	int currentIndex;
+	int size;
+} PQueue;
+
+void reheapUp(PQueue* pQueue, int currentIndex)
+{
+	if (currentIndex > 0)
+	{
+		int parentIndex = (currentIndex - 1) / 2;
+
+		if (pQueue->items[parentIndex]->size < pQueue->items[currentIndex]->size)
+		{
+			File* tmp = pQueue->items[parentIndex];
+			pQueue->items[parentIndex] = pQueue->items[currentIndex];
+			pQueue->items[currentIndex] = tmp;
+
+			reheapUp(pQueue, parentIndex);
+		}
+	}
+}
+
+void insertIntoHeap(PQueue* pQueue, File* file)
+{
+	if (pQueue->currentIndex >= pQueue->size)
+	{
+		pQueue->size *= 2;
+		pQueue->items = (File**)realloc(pQueue->items, sizeof(File*) * pQueue->size);
+	}
+
+	pQueue->items[pQueue->currentIndex] = file;
+	reheapUp(pQueue, pQueue->currentIndex);
+	pQueue->currentIndex++;
+}
+
+void enqueueFilesByOwner(TreeNode* root, PQueue* pQueue, const char* owner)
+{
+	if (root == NULL)
+	{
+		return;
+	}
+
+	if (strcmp(root->info->owner, owner) == 0)
+	{
+		// create a new file copy in order for the two structures
+		// to NOT share heap memory space
+		File* copy = createFile(root->info->identifier, root->info->size, root->info->readOnly,
+			root->info->createdDate, root->info->owner);
+		insertIntoHeap(pQueue, copy);
+	}
+
+	enqueueFilesByOwner(root->left, pQueue, owner);
+	enqueueFilesByOwner(root->right, pQueue, owner);
+}
 
 void main()
 {
+	// MEMORY IS NOT DEALLOCATED IN THIS IMPLEMENTATION
+
 	TreeNode* root = NULL;
 
 	FILE* pFile = fopen("DataFile.txt", "r");
@@ -231,15 +410,32 @@ void main()
 	displayTree(root, 0);
 
 	// 2.
-	displayGreaterThanDate(root, "2023/12/13");
+	//displayGreaterThanDate(root, "2023/12/13");
 
 	// 3.
 	int filesReadOnly = 0;
 	countFilesReadOnly(root, &filesReadOnly);
-	printf("\nNum of files with read only access: %d\n", filesReadOnly);
+	//printf("\nNum of files with read only access: %d\n", filesReadOnly);
 
 	// 4.
 	int filesCount = 0;
 	double avgSize = avgFileSize(root, &filesCount);
-	printf("\nAvg file size: %.2f\n", avgSize);
+	//printf("\nAvg file size: %.2f\n", avgSize);
+
+	// 5.
+	increaseSizePath(root, "103C");
+	printf("\n--------------------------------------\n");
+	displayTree(root, 0);
+
+	// 6.
+	PQueue pQueue = { .items = NULL, .currentIndex = 0, .size = PQUEUE_SIZE };
+	pQueue.items = (File**)malloc(sizeof(File*) * PQUEUE_SIZE);
+
+	enqueueFilesByOwner(root, &pQueue, "Ruxandra Iliescu");
+	printf("\n--------------------------------------\n");
+	printf("\nHeap elements:\n");
+	for (int i = 0; i < pQueue.currentIndex; i++)
+	{
+		displayFile(pQueue.items[i]);
+	}
 }
